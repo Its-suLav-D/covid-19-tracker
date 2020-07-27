ede16000ee3aeae9ad9b31b8ce7a7a4fbbdbbe3d
Its-suLav-D <dah19001@byui.edu>
2020-07-26 18:37:18 -0700

    Updates

---
+++ precache-manifest.fb82a70710b476a0c036bb20144023ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b423087fd8d68e45f2c868a84f6b3e",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "aac638bea4a0525e6e9c",
+    "url": "/covid-19-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "0c9afcf9ab633268d9e7",
+    "url": "/covid-19-tracker/static/css/main.a9d6bdb6.chunk.css"
+  },
+  {
+    "revision": "aac638bea4a0525e6e9c",
+    "url": "/covid-19-tracker/static/js/2.f5beb738.chunk.js"
+  },
+  {
+    "revision": "9922ea9069e02abaf7aeceb5abadc3c8",
+    "url": "/covid-19-tracker/static/js/2.f5beb738.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c9afcf9ab633268d9e7",
+    "url": "/covid-19-tracker/static/js/main.c6830071.chunk.js"
+  },
+  {
+    "revision": "fb403f6422f9b7442ea3",
+    "url": "/covid-19-tracker/static/js/runtime-main.960a8408.js"
+  }
+]);
